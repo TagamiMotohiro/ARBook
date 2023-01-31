40c8c853d25adadddabec7ee9127bc4f78722d79
TagamiMotohiro <105412324+TagamiMotohiro@users.noreply.github.com>
2023-01-31 15:22:12 +0900

    初期状態

---
+++ ARBook/Assets/BookDebug.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookDebug : MonoBehaviour
+{
+    public void OutPut()
+    {
+        Debug.Log("本が開いてパンダが現れた");
+    }
+}
